13fd9f1f1054b9d9699b2571469b3a6dbfb638f9
xu.lei <xu.lei@cesgroup.com.cn>
2018-07-25 12:47:27 +0800

    add spring boot admin

--- base-module/eureka-server/src/main/java/com/example/usercenter/eureka/server/EurekaServerApplication.java
+++ base-module/eureka-server/src/main/java/com/example/usercenter/eureka/server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.usercenter.eureka;
+package com.example.usercenter.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
